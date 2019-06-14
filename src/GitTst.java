a98b0fa7d73e3f232b547a5d555aceedc5f966a8
admin <admin@DESKTOP-LQPNNU1>
2019-06-14 19:59:35 +0800

    第二次提交

--- src/GitTst.java
+++ src/GitTst.java
@@ -3,7 +3,8 @@ public class GitTst {
 
 	public static void main(String[] args) {
 		System.out.println("aaaaaaa");
-                System.out.println("bbbbbbbbb");
+        System.out.println("bbbbbbbbb");
+        System.out.println("CCCCCCCCCCCCCC");
 	}
 
 }
